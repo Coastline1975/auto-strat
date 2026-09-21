bcf8c5c1e9ea68809d0045f80cce4b02cb3d42b0
hfjghhfhghfhjgjghfhffgghhjghfgfjgfgjhfhj <74459442+Coastline1975@users.noreply.github.com>
2026-09-21 07:34:07 +0500

    Update Recorder.lua
    swap links [fix recorder link]

--- Sources/Recorder.lua
+++ Sources/Recorder.lua
@@ -792,7 +792,7 @@ return function(ctx)
                         game_info_str = string.format('\nTDS:GameInfo("%s", {%s})', current_map, current_modifiers)
                     end
                     local config_header = string.format([[
-local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/Coastline1975/auto-strat/refs/heads/main/Library.lua"))()
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/Coastline1975/auto-strat/refs/heads/Coastline1975-patch-1/Library.lua"))()
 
 TDS:Loadout("%s", "%s", "%s", "%s", "%s")
 TDS:Mode("%s")%s
